f9eedb61006402b2983fbb3109f8af9a96519f2d
Jeremy Brautzsch <jeremybrautzsch@Jeremys-MBP.fritz.box>
2020-03-02 19:48:57 +0100

    test

--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -1,10 +1,10 @@
 import { LightningElement, track } from 'lwc';
 import { getSessions } from 'data/sessionService';
 export default class SessionList extends LightningElement {
-  @track sessions = [];
-  connectedCallback() {
-    getSessions().then(result => {
-      this.sessions = this.allSessions = result;
-    });
-  }
+    @track sessions = [];
+    connectedCallback() {
+        getSessions().then(result => {
+            this.sessions = this.allSessions = result;
+        });
+    }
 }
